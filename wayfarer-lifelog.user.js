c63b53f91b05f53d5747a4aad74e87315191adb3
wiinuk <wii.nukegara@gmail.com>
2021-12-16 23:26:07 +0900

    修正

--- wayfarer-lifelog.user.js
+++ wayfarer-lifelog.user.js
@@ -1,16 +1,19 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         Wayfarer Lifelog
+// @namespace    https://github.com/wiinuk/wayfarer-scripts
+// @downloadURL  https://github.com/wiinuk/wayfarer-scripts/raw/main/wayfarer-lifelog.user.js
+// @homepageURL  https://github.com/wiinuk/wayfarer-scripts
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://wayfarer.nianticlabs.com/new/profile
+// @description  Add LifeLog to Wayfarer
+// @author       Wiinuk
+// @match        https://wayfarer.nianticlabs.com/*
 // @icon         https://www.google.com/s2/favicons?domain=nianticlabs.com
 // @grant        none
 // ==/UserScript==
 
+//@ts-check
+
 (function() {
     'use strict';
-
-    // Your code here...
+    
 })();
