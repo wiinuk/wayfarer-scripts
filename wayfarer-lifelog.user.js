acd5e48f7591aabe91b1ab86e4b5a24cc57f0793
wiinuk <wii.nukegara@gmail.com>
2021-12-16 23:16:29 +0900

    first commit

---
+++ wayfarer-lifelog.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://wayfarer.nianticlabs.com/new/profile
+// @icon         https://www.google.com/s2/favicons?domain=nianticlabs.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
