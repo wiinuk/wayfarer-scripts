0a6d165b9441eeaa58b84297c38a531bb6b8f195
wiinuk <wii.nukegara@gmail.com>
2021-12-29 21:44:27 +0900

    修正

--- commit.sh
+++ commit.sh
@@ -1,8 +1,5 @@
-git add -N .
-if ! git diff --exit-code --quiet; then
-  git config user.name github-actions
-  git config user.email github-actions@github.com
-  git add .
-  git commit -m "Auto commit by github-actions"
-  git push
-fi
+git add --update
+git config user.name github-actions
+git config user.email github-actions@github.com
+git commit -m "Auto commit by github-actions"
+git push
